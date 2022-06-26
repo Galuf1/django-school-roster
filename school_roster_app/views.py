20307e12949ae017ff07cf5b18d74994eed2dc99
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-06-26 17:54:23 +0200

    no css

--- school_roster_app/views.py
+++ school_roster_app/views.py
@@ -8,16 +8,39 @@ def index(request):
     my_data = {
         "school_name": my_school.name
     }
-    return render(request, "pages/index.html", my_data)
+    
+    return render(request, "school_roster_app/index.html", my_data)
 
 def list_staff(request):
-    pass
+    
+    my_data = {
+        "school_name": my_school.name,
+        "staff": my_school.staff
+    }
 
-def staff_detail(request):
-    pass
+    return render(request, "school_roster_app/list_staff.html", my_data)
+
+def staff_detail(request, employee_id):
+    staff = my_school.find_staff_by_id(employee_id)
+    my_data = {
+        "school_name": my_school.name,
+        "staff": staff
+    }
+            
+    return render(request, "school_roster_app/staff_detail.html", my_data)
 
 def list_students(request):
-    pass
+    my_data = {
+        "school_name": my_school.name,
+        "students": my_school.students
+    }
+    return render(request,"school_roster_app/list_students.html", my_data )
 
-def student_detail(request):
-    pass
+def student_detail(request, student_id):
+    student = my_school.find_student_by_id(student_id)
+
+    my_data = {
+        "School_name": my_school.name,
+        "student": student
+    }
+    return render(request, "school_roster_app/student_detail.html", my_data)
